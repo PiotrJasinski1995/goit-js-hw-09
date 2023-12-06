9d9edd0ee04ff7630eed87559c05f53b17eef3b6
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2023-12-06 23:13:25 +0100

    feat: implement task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const bodyElement = document.querySelector('body');
+let timerId = null;
 
+startButton.addEventListener('click', event => {
+  event.currentTarget.disabled = true;
+  timerId = setInterval(() => {
+    const randomBackgroundColor = getRandomHexColor();
+    bodyElement.style.backgroundColor = randomBackgroundColor;
+  }, 1000);
+});
+
+stopButton.addEventListener('click', event => {
+  startButton.disabled = false;
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
